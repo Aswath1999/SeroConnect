f373fa099d1b9e36bd520b2c76f26f45bddd160c
DESKTOP-O2UQO9I\Aswath <aswath.chandrasekaran@stud.th-deg.de>
2022-06-27 16:47:57 +0200

    Added userprofile page

--- website/views.py
+++ website/views.py
@@ -26,3 +26,13 @@ def forum():
     if "hx_request"  in request.headers:
         return render_template("forum/viewpost.html",user=current_user,postuser=User,posts=posts,postcomment=Comment,postimage=Image)
     return render_template("forum/forum.html",user=current_user,postuser=User,posts=posts,postcomment=Comment,postimage=Image)
+
+@views.route('/userprofile',methods=['GET', 'POST'])
+@login_required
+@check_confirmed
+def userprofile():
+    page=request.args.get('page',1,type=int)
+    posts=Post.query.filter(Post.user_id==current_user.id).paginate(page=page,per_page=4)
+    if "hx_request"  in request.headers:
+        return render_template("forum/viewpost.html",user=current_user,postuser=User,posts=posts,postcomment=Comment,postimage=Image)
+    return render_template('profile.html',user=current_user,posts=posts,postuser=User,postcomment=Comment,postimage=Image)
